d363cf41c67cb6b97c48d45c0515d43b964d54a3
oka-saputra <okadanil@gmail.com>
2020-02-06 16:48:51 +0700

    Automation program supports to create screenshot

--- src/test/java/com/gdti/Consultation.java
+++ src/test/java/com/gdti/Consultation.java
@@ -1,21 +1,26 @@
 package com.gdti;
 
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.AutomationName;
 import io.appium.java_client.remote.MobileCapabilityType;
-import jdk.internal.instrumentation.Logger;
-import org.openqa.selenium.logging.LogEntry;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.By;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.net.MalformedURLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 
@@ -24,6 +29,7 @@ public class Consultation {
     private String reportFormat = "xml";
     private String testName = "Consultation";
     protected AndroidDriver<AndroidElement> driver = null;
+    private static int latestId = 0;
     DesiredCapabilities dc = new DesiredCapabilities();
 
     @BeforeMethod(alwaysRun = true)
@@ -33,7 +39,8 @@ public class Consultation {
         dc.setCapability("reportDirectory", reportDirectory);
         dc.setCapability("reportFormat", reportFormat);
 //        dc.setCapability(MobileCapabilityType.UDID, "RR8M50AK3YL");
-        dc.setCapability("deviceId", "10.143.20.30:5555");
+        dc.setCapability(MobileCapabilityType.UDID, "HQHY6HOFQWPRWWHU");
+//        dc.setCapability("deviceId", "10.143.20.30:5555");
         dc.setCapability(MobileCapabilityType.DEVICE_NAME, "SamsungA20");
         dc.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         dc.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.ANDROID_UIAUTOMATOR2);
@@ -47,23 +54,54 @@ public class Consultation {
     @Test
     public void testConsultation() {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        try{Thread.sleep(10000);} catch(Exception ignore){}
+        takeScreenShot();
         driver.findElement(By.xpath("//android.widget.TextView[@text='More']")).click();
+        try{Thread.sleep(500);} catch(Exception ignore){}
+        takeScreenShot();
         driver.findElement(By.xpath("//android.widget.TextView[@text='Health']")).click();
         try{Thread.sleep(12000);} catch(Exception ignore){}
+        takeScreenShot();
         driver.findElement(By.xpath("//android.view.View[contains(@text,'Consultation')]")).click();
+        try{Thread.sleep(500);} catch(Exception ignore){}
+        takeScreenShot();
         driver.findElement(By.xpath("//android.view.View[contains(@text,'Free')]")).click();
         try{Thread.sleep(1500);} catch(Exception ignore){}
+        takeScreenShot();
         driver.findElement(By.xpath("//android.view.View[@text='Confirm']")).click();
         try{Thread.sleep(5000);} catch(Exception ignore){}
+        takeScreenShot();
         driver.findElement(By.xpath("//android.widget.Button[@text='End']")).click();
         try{Thread.sleep(2000);} catch(Exception ignore){}
+        takeScreenShot();
         driver.findElement(By.xpath("//android.widget.Button[contains(@text,'Yes')]")).click();
         try{Thread.sleep(2000);} catch(Exception ignore){}
+        takeScreenShot();
+        try{Thread.sleep(1000);} catch(Exception ignore){}
     }
 
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
         driver.quit();
+    }
+
+    public void takeScreenShot() {
+        // Set folder name to store screenshots.
+        String destDir = "screenshots";
+        // Capture screenshot.
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        // Create folder under project with name "screenshots" provided to destDir.
+        new File(destDir).mkdirs();
+        try {
+            // Copy paste file at destination folder location
+            FileUtils.copyFile(scrFile, new File(destDir + "/Automation-" + generateId() + ".png" ));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int generateId() {
+        return ++latestId;
     }
 
 }
